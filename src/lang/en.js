a235b8f37c38a254acbeeeccb8104689ab464fd4
alex1234567639 <alex8603062000@gmail.com>
2022-11-13 20:20:45 +0800

    [feature]新增i18n

---
+++ src/lang/en.js
@@ -0,0 +1,12 @@
+export default {
+  login: {
+    title: 'Login Form',
+    logIn: 'Login',
+    username: 'Username',
+    password: 'Password',
+    captcha: 'Verification Code',
+    wrongCaptcha: 'Wrong Verification Code',
+    loginFail: 'Login failed. Please try it again.',
+    password6Digits: 'The password can not be less than 6 digits.'
+  }
+}
