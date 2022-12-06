cd5911b1f22381e02411ecb1087ae47304f1c277
alex1234567639 <alex8603062000@gmail.com>
2022-12-06 15:13:09 +0800

    [feature]新增會員相關

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -16,7 +16,7 @@ export default {
     captcha: '驗證碼',
     wrongCaptcha: '驗證碼錯誤',
     loginFail: '帳號或密碼錯誤，請重新再試',
-    password6Digits: '密碼長度不得低於6個字'
+    password6Digits: '密碼長度介於8~16字'
   },
   // Navbar
   navbar: {
@@ -41,7 +41,7 @@ export default {
     manager: '管理者',
     blogAdminUser: '前後台用戶',
     blogOnlyUser: '前台用戶',
-    inputMame: '輸入用戶名',
+    inputName: '輸入用戶名',
     inputAccount: '輸入帳號',
     search: '搜尋',
     no: '編號',
@@ -50,6 +50,34 @@ export default {
     account: '帳號',
     createDate: '註冊日期',
     type: '類型',
-    status: '狀態'
+    status: '狀態',
+    // 新增會員
+    addAccount: '新增會員',
+    password: '密碼',
+    inputPassword: '輸入密碼',
+    passwordConfirm: '確認密碼',
+    inputPasswordConfirm: '確認密碼',
+    inputValidPassword: '請輸入正確密碼格式',
+    differentPassword: '密碼不一致'
+  },
+  alert: {
+    alert: '提醒',
+    send: '送出',
+    confirm: '確定',
+    cancel: '取消',
+    fileTooLarge: '檔案大小不得超過1M',
+    sizeTooLarge: '檔案尺寸不得超過1920*1000',
+    addSuccess: '新增成功',
+    editSuccess: '修改成功',
+    deleteSuccess: '刪除成功'
+  },
+  lightbox: {
+    cancel: '取消',
+    confirm: '確定',
+    add: '新增',
+    search: '搜尋',
+    reLogin: '重新登入',
+    reLoginTitle: '重新登入提示',
+    reLoginContent: '此帳號已閒置過久或已從其他地方登入，請選擇重新登入或是留在此頁。'
   }
 }
