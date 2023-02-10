dbfac23e4c37df885281f787351b2fa0fa339e62
alex1234567639 <alex8603062000@gmail.com>
2023-02-10 22:17:38 +0800

    [feature]富文本範例

---
+++ src/views/tinymceDemo/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tinymce-demo-wrapper">
+    <el-button class="btn" type="primary" @click="showContent">取得副文本內容</el-button>
+    <tinymce v-model="content" :height="300" />
+
+    <h3>預覽</h3>
+    <div v-html="content" />
+  </div>
+</template>
+
+<script>
+import Tinymce from '@/components/Tinymce'
+
+export default {
+  components: { Tinymce },
+  data() {
+    return {
+      content: ''
+    }
+  },
+  methods: {
+    showContent() {
+      console.log(this.content)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tinymce-demo-wrapper {
+  padding: 20px;
+  & .btn {
+    margin-bottom: 10px;
+  }
+}
+</style>
