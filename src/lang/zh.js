03a6bfea92b73e5479c843cd647cff0fd9d8e127
alex1234567639 <alex8603062000@gmail.com>
2023-02-10 23:29:59 +0800

    [delete]刪除富文本範例

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -5,9 +5,7 @@ export default {
     // 會員管理
     adminManage: '會員管理',
     adminList: '會員列表',
-    adminAdd: '新增會員',
-    // 富文本範例
-    tinymce: '富文本範例'
+    adminAdd: '新增會員'
   },
   // 登入
   login: {
