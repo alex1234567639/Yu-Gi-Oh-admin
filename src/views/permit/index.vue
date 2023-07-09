042b8f1b98ea6e794625e4e83b7a8f6eededcaec
alex1234567639 <alex8603062000@gmail.com>
2023-07-09 20:08:01 +0800

    [test]api測試

--- src/views/permit/index.vue
+++ src/views/permit/index.vue
@@ -3,3 +3,19 @@
     permit-wrapper
   </div>
 </template>
+
+<script>
+import { callApi } from '@/api/api'
+// import { getPermit } from '@/api/permission'
+
+export default {
+  mounted() {
+    callApi('cards', 'list').then((res) => {
+      console.log(res)
+    })
+    // getPermit().then(res => {
+    //   console.log(res)
+    // })
+  }
+}
+</script>
