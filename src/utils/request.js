ab18bd5709aaf9dfbe711e585a8435f58ceb3b9d
alex1234567639 <alex8603062000@gmail.com>
2023-07-10 23:23:23 +0800

    [modify]印出request & response

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
+import { decode } from '@/utils/decode'
 import i18n from '@/lang'
 
 // create an axios instance
@@ -15,11 +16,7 @@ const service = axios.create({
 service.interceptors.request.use(
   (config) => {
     // do something before request is sent
-
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
       config.headers['X-Token'] = getToken()
     }
     return config
@@ -34,7 +31,19 @@ service.interceptors.request.use(
 // response interceptor
 service.interceptors.response.use(
   (response) => {
-    console.log(response)
+    // 測試環境顯示 request、response 的 console
+    if (store.state.settings.showLog) {
+      console.log({
+        'url': response.config.url.replace(response.config.baseURL, ''),
+        'requestData': decode(JSON.parse(response.config.data).data)
+      })
+      console.log({
+        'error_code': response.data.error_code,
+        'responseData': decode(response.data.data),
+        'status': response.status
+      })
+    }
+
     const res = response.data
     if (res.error_code && res.error_code !== 10009) {
       Message({
@@ -44,7 +53,6 @@ service.interceptors.response.use(
       })
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
-      // Success
       if (res.error_code) {
         Message({
           message: i18n.t(`errorCode.${res.error_code}`),
