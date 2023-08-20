8cc6ec4658cbd53fac541842fd6186fb0b44953d
alex1234567639 <alex8603062000@gmail.com>
2023-08-20 15:32:44 +0800

    [modify]調整為 ES6 的語法

--- src/config/main.js
+++ src/config/main.js
@@ -1,6 +1,22 @@
-module.exports = {
-  // 上傳照片限制
-  KB_limit: 1024,
-  width_limit: 1920,
-  height_limit: 1000
+import i18n from '@/lang'
+
+// 上傳照片限制
+const KB_limit = 1024
+const width_limit = 1920
+const height_limit = 1000
+
+// 產品包裝分類
+const packMainTypeList = [
+  { value: 0, label: i18n.t('route.pack') },
+  { value: 1, label: i18n.t('route.rushDuel') },
+  { value: 3, label: i18n.t('route.deck') },
+  { value: 4, label: i18n.t('route.box') },
+  { value: 2, label: i18n.t('route.other') }
+]
+
+export {
+  KB_limit,
+  width_limit,
+  height_limit,
+  packMainTypeList
 }
