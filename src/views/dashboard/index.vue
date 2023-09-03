c3b38704c36504a7eaed6365ee7ed933bb5bde02
alex1234567639 <alex8603062000@gmail.com>
2023-09-03 12:48:21 +0800

    [delete]刪除沒用到的部分

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,70 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole" />
+    <span class="text1">Welcome!</span>
+    <span class="text2">Welcome!</span>
+    <span class="text3">Welcome!</span>
+    <span class="text4">Welcome!</span>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
 
 export default {
   name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
   data() {
     return {
-      currentRole: 'adminDashboard'
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'roles'
-    ])
-  },
-  created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  height: 300px;
+  text-align: center;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, .7);
+  padding-top: 90px;
+  overflow: hidden;
+  position: relative;
+  .text1 {
+    position: absolute;
+    top: 80px;
+    left: 280px;
+    font-size: 17em;
+    color: rgba(0, 0, 0, .08);
+    font-weight: bold;
+  }
+  .text2 {
+    position: absolute;
+    top: -50px;
+    left: -60px;
+    font-size: 17em;
+    color: rgba(0, 0, 255, .05);
+    font-weight: bold;
+  }
+  .text3 {
+    position: absolute;
+    top: -120px;
+    left: 550px;
+    font-size: 17em;
+    color: rgba(255, 0, 0, .05);
+    font-weight: bold;
+  }
+  .text4 {
+    font-size: 5em;
+    font-weight: bold;
+    font-family: Righteous;
+    color: rgba(0, 0, 0, .7);
+    &:after {
+      content: "";
+      display: block;
+      background-color: rgba(0, 0, 0, .4);
+      height: 2px;
+      width: 400px;
+      border-radius: 50%;
+      margin: 5px auto;
+    }
+  }
+}
+</style>
