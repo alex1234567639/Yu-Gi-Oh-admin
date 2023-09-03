1709bb4eede1e4a821d4d8378a7d6b11c5e8fc10
alex1234567639 <alex8603062000@gmail.com>
2023-09-03 20:45:18 +0800

    [modify]去掉用不到的部分

--- src/api/article.js
+++ src/api/article.js
@@ -1,46 +1,5 @@
-import request from '@/utils/request'
 import store from '@/store'
 import { callApi } from './api'
-
-export function fetchList(query) {
-  return request({
-    url: '/vue-element-admin/article/list',
-    method: 'get',
-    params: query
-  })
-}
-
-export function fetchArticle(id) {
-  return request({
-    url: '/vue-element-admin/article/detail',
-    method: 'get',
-    params: { id }
-  })
-}
-
-export function fetchPv(pv) {
-  return request({
-    url: '/vue-element-admin/article/pv',
-    method: 'get',
-    params: { pv }
-  })
-}
-
-export function createArticle(data) {
-  return request({
-    url: '/vue-element-admin/article/create',
-    method: 'post',
-    data
-  })
-}
-
-export function updateArticle(data) {
-  return request({
-    url: '/vue-element-admin/article/update',
-    method: 'post',
-    data
-  })
-}
 
 export async function checkTagList(list) {
   if (!Array.isArray(list)) return
